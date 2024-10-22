f6caeec95e6096a09d36f90468db2b7d81418672
Dipayan Roy <roy.dipayan131@gmail.com>
2024-10-22 20:38:55 +0530

    new changes

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const connectDB = require('./config/db');
-const userRoutes = require('./routes/userRoutes');
+const connectDB = require('../config/db');
+const userRoutes = require('../routes/userRoutes');
 require('dotenv').config();
 
 const app = express();
@@ -19,3 +19,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+export default app;
